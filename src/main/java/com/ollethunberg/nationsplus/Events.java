1ae1c3b10de518bf9ed78bb15b4462feab88d847
Olle Thunberg <olle@fls.se>
2022-07-16 17:19:05 +0200

    Prefix implementation based on which nation you are part of  #2

--- src/main/java/com/ollethunberg/nationsplus/Events.java
+++ src/main/java/com/ollethunberg/nationsplus/Events.java
@@ -2,67 +2,101 @@ package com.ollethunberg.nationsplus;
 
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.Plugin;
-import java.sql.*;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.HashMap;
 
 public class Events implements Listener {
 
-    private Connection conn;
+    private SQLHelper sqlHelper;
     private Plugin plugin = NationsPlus.getPlugin(NationsPlus.class);
+
+    HashMap<String, String> prefixCache = new HashMap<String, String>();
 
     public Events(Connection _connection) {
 
-        conn = _connection;
+        sqlHelper = new SQLHelper(_connection);
     }
 
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
 
         try {
-            plugin.getLogger().info("Player joined!" + conn.toString());
+
             // Check if they are in the database.
             String isPlayerInDatabaseSQL = "SELECT EXISTS ( SELECT FROM player WHERE uid = ? );";
-            PreparedStatement preparedStatement = conn.prepareStatement(isPlayerInDatabaseSQL);
-            preparedStatement.setString(1, event.getPlayer().getUniqueId().toString());
+            ResultSet rs = sqlHelper.query(isPlayerInDatabaseSQL, event.getPlayer().getUniqueId().toString());
 
-            ResultSet rs = preparedStatement.executeQuery();
             rs.next();
 
             if (!rs.getBoolean("exists")) {
                 plugin.getLogger().info("New player joined!");
                 // Insert into database
                 String insertNewPlayerSQL = "INSERT INTO player(uid, last_login, player_name, kills, deaths) VALUES (?, CURRENT_TIMESTAMP, ?, 0,0);";
-                PreparedStatement prepareInsertStatement = conn.prepareStatement(insertNewPlayerSQL);
-                prepareInsertStatement.setString(1, event.getPlayer().getUniqueId().toString());
-                prepareInsertStatement.setString(2, event.getPlayer().getDisplayName());
-                prepareInsertStatement.executeUpdate();
+                sqlHelper.update(insertNewPlayerSQL, event.getPlayer().getUniqueId().toString(),
+                        event.getPlayer().getName());
+                // Teleport the player to the spawn
             } else {
                 plugin.getLogger().info("Player does exist in the database!");
                 // Check if the player has a ban on them on the player_bans table
-                String playerBannedUntil = "SELECT banned_date + (banned_minutes * interval '1 minute') as banned_until FROM player_bans WHERE player_id = ? order by banned_date DESC;";
-                PreparedStatement preparePlayerBannedStatement = conn.prepareStatement(playerBannedUntil);
-                preparePlayerBannedStatement.setString(1, event.getPlayer().getUniqueId().toString());
-                ResultSet rsPlayerBannedUntil = preparePlayerBannedStatement.executeQuery();
+                String playerBannedUntil = "SELECT banned_date + (banned_minutes * interval '1 minute') as banned_until, player_id FROM player_bans WHERE player_id = ? order by banned_date DESC;";
+                ResultSet rsPlayerBannedUntil = sqlHelper.query(playerBannedUntil,
+                        event.getPlayer().getUniqueId().toString());
                 rsPlayerBannedUntil.next();
-                if (rsPlayerBannedUntil.getTimestamp("banned_until") != null) {
-                    if (rsPlayerBannedUntil.getTimestamp("banned_until")
-                            .after(new Timestamp(System.currentTimeMillis()))) {
-                        plugin.getLogger().info("Player is banned!");
-                        event.getPlayer().kickPlayer("§cYou are not able to join the server, ");
-                        return;
-                    }
-                }
+                /*
+                 * if (rsPlayerBannedUntil.getTimestamp("banned_until") != null) {
+                 * if (rsPlayerBannedUntil.getTimestamp("banned_until")
+                 * .after(new Timestamp(System.currentTimeMillis()))) {
+                 * plugin.getLogger().info("Player is banned!");
+                 * event.getPlayer().kickPlayer("§cYou are not able to join the server, ");
+                 * return;
+                 * }
+                 * } else {
+                 * // log that the player is not banned
+                 * plugin.getLogger().info("Player is not banned!");
+                 * }
+                 */
                 String updatePlayerLastLoginSQL = "UPDATE player SET last_login=CURRENT_TIMESTAMP, player_name=? where uid = ?";
-                PreparedStatement prepareUpdateStatement = conn.prepareStatement(updatePlayerLastLoginSQL);
-                prepareUpdateStatement.setString(1, event.getPlayer().getDisplayName());
-                prepareUpdateStatement.setString(2, event.getPlayer().getUniqueId().toString());
-                prepareUpdateStatement.executeUpdate();
+                sqlHelper.update(updatePlayerLastLoginSQL, event.getPlayer().getDisplayName(),
+                        event.getPlayer().getUniqueId().toString());
+                // Update our prefix cache with the prefix of the nation that the player is in.
+                String getPlayerNationSQL = "SELECT n.prefix FROM player as p inner join nation as n on p.nation=n.name WHERE p.uid = ?;";
+                ResultSet rsPlayerNation = sqlHelper.query(getPlayerNationSQL,
+                        event.getPlayer().getUniqueId().toString());
+                if (rsPlayerNation.next()) {
+                    prefixCache.put(event.getPlayer().getUniqueId().toString(), rsPlayerNation.getString("prefix"));
+                } else {
+                    plugin.getLogger().info("Player is not in a nation!");
+                    event.getPlayer().sendMessage("§aPlease join a nation!");
+                }
+
             }
 
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
 
+    }
+
+    @EventHandler
+    public void onPlayerQuit(PlayerQuitEvent event) {
+        // remove player from cache
+        prefixCache.remove(event.getPlayer().getUniqueId().toString());
+    }
+
+    @EventHandler
+    public void onPlayerChat(AsyncPlayerChatEvent e) {
+        // Get preifix from the cache.
+        String prefix = prefixCache.get(e.getPlayer().getUniqueId().toString());
+        if (prefix != null) {
+            e.setFormat("§e[§r" + prefix + "§e]§r %s : %s");
+        }
     }
 }
