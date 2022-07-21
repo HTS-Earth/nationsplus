7e5dc6b604d1063ff3f7cc32b2ab1016d919f306
Olle Thunberg <olle@fls.se>
2022-07-21 17:26:39 +0200

    You can now overthrow a nation

--- src/main/java/com/ollethunberg/nationsplus/CrownClaimCommand.java
+++ src/main/java/com/ollethunberg/nationsplus/CrownClaimCommand.java
@@ -49,21 +49,51 @@ public class CrownClaimCommand extends DatabaseInteractor {
                     .get(p.getInventory().getItemInMainHand().getItemMeta().getLore().size() - 1);
 
             // Check if the player is in a nation
-            String isInNationSQLString = "SELECT nation FROM player WHERE uid = ? and nation = ?";
+            String isInNationSQLString = "SELECT nation FROM player WHERE uid = ?";
             try {
-                ResultSet rs = sqlHelper.query(isInNationSQLString, p.getUniqueId().toString(), nationName);
+                ResultSet rs = sqlHelper.query(isInNationSQLString, p.getUniqueId().toString());
+                // Check if the player is in the nation
                 rs.next();
                 // if the Player is part of the nation, the claim the crown and set the king_id
                 // to the players uid
-                if (rs.getString("nation") != null) {
+                if (rs.getString("nation") != null && rs.getString("nation").equals(nationName)) {
                     String claimCrownSQLString = "UPDATE nation SET king_id = ? WHERE name = ?";
                     sqlHelper.update(claimCrownSQLString, p.getUniqueId().toString(), rs.getString("nation"));
 
                     p.sendMessage("§aYou have claimed the crown!");
-
                 } else {
-                    p.sendMessage("§cYou are not in the nation! Therefore, you can't claim the crown");
+                    // See if there is a nation with the nation name
+                    String getNationSQLString = "SELECT name FROM nation WHERE name = ?";
+                    ResultSet nationRs = sqlHelper.query(getNationSQLString, nationName);
+                    if (nationRs.next()) {
+
+                        String nation = nationRs.getString("name");
+                        // Set the nation king_id to be null
+                        String unclaimCrownSQLString = "UPDATE nation SET king_id = null WHERE name = ?";
+                        sqlHelper.update(unclaimCrownSQLString, nation);
+                        // Announce that the nation has fallen
+
+                        // Make all the players that belonged to the nation nationless
+                        String makeNationlessSQLString = "UPDATE player SET nation = null WHERE nation = ?";
+                        sqlHelper.update(makeNationlessSQLString, nation);
+                        // Remove the relationships of the nation
+                        String removeRelationshipsSQLString = "DELETE FROM nation_relations WHERE nation_one = ? or nation_second = ?;";
+                        sqlHelper.update(removeRelationshipsSQLString, nation, nation);
+                        // Remove the nation
+                        String removeNationSQLString = "DELETE FROM nation WHERE name = ?";
+                        sqlHelper.update(removeNationSQLString, nation);
+                        // Remove the crown from the players main hand
+                        p.getInventory().setItemInMainHand(new ItemStack(Material.AIR));
+
+                        plugin.getServer()
+                                .broadcastMessage("§c§lThe nation " + nation + " has fallen!");
+                        plugin.getServer().broadcastMessage("§aAll its members are not nationless");
+                    } else {
+                        p.sendMessage("§c§lThere is no nation that belongs to this crown!");
+                    }
+
                 }
+
             } catch (SQLException e) {
                 e.printStackTrace();
                 // Message the user that something went wrong claiming the crown
